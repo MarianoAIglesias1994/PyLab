de330337a22fa1920df909f4b85456a70321cf9c
MarianoAIglesias1994 <marianoiglesias1994@hotmail.com>
2017-06-20 02:20:11 -0300

    Agrego ayudas

--- Clasificadores_HM/demo.py
+++ Clasificadores_HM/demo.py
@@ -7,13 +7,13 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
-     'female', 'male', 'male']
+Y = ['male', 'male', 'child', 'child', 'male', 'male', 'female', 'female',
+     'child', 'male', 'male']
 
 clf = clf.fit(X, Y)
 
-prediction = clf.predict([[175, 75, 40]])
-proba = clf.predict_proba([[175, 75, 40]])
+prediction = clf.predict([[180, 75, 42]])
+proba = clf.predict_proba([[180, 75, 42]])
 
 print(prediction)
 print(proba)
